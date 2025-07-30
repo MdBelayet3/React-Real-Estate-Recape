9fd582d03db4d9b5e2077295d36d35b7af94e395
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-30 13:20:42 +0600

    Update PaymentDetails.jsx

--- src/pages/Payment/PaymentDetails.jsx
+++ src/pages/Payment/PaymentDetails.jsx
@@ -6,7 +6,7 @@ const PaymentDetails = ({ payment }) => {
     return (
         <div className='flex justify-center'>
             <div className="tooltip" data-tip={payment?.name}>
-                <Link to={payment?.link}><button className=""><img className=' h-[200px] w-[200px] rounded-2xl' src={payment?.image} alt="" /></button></Link>
+                <Link to={payment?.link}><button className=""><img className=' h-[200px] w-[200px] rounded-3xl' src={payment?.image} alt="" /></button></Link>
             </div>
         </div>
     );
