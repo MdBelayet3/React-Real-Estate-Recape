c1b5c3b1e539c83a5bd37a9483e7afd64d309211
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-08-06 03:18:34 +0600

    Update Navbar.jsx

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box text-black z-[1] mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box text-black z-[1] mt-4 w-52 p-2 shadow">
                         {nabLink}
                     </ul>
                 </div>
